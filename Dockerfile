3d25098ba4ef7c3fb3b5093dc90887cdb6578c1b
Kybik0606 <kybik0606@gmail.com>
2026-04-24 13:31:37 +0300

    add docker files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY requirements.txt  .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+VOLUME ["/app/results"]
+
+EXPOSE 5000
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def admin_results():
     return render_template('admin.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
